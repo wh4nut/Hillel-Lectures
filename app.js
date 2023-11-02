426d9ec62339173cd69fe89f62f9b17ff406afed
wh4nut <wh4nut@gmail.com>
2023-11-02 22:05:41 +0200

    Hilel 2

---
+++ app.js
@@ -0,0 +1,11 @@
+const firstNumber = Number(prompt('Write first number'));
+console.log(typeof firstNumber, firstNumber);
+
+const secondNumber = Number(prompt('Write second number'));
+console.log(typeof secondNumber, secondNumber);
+
+const thirdNumber = Number(prompt('Write third number'));
+console.log(typeof thirdNumber, thirdNumber);
+
+const average = (firstNumber + secondNumber + thirdNumber) / 3;
+console.log(typeof average, average);
