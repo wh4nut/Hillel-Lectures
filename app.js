c81a2fd24051138d775a3a0f3e77d2175d412e18
Kyrylo Sudakov <wh4nut@gmail.com>
2023-11-06 22:36:22 +0200

    Hillel - 3

--- app.js
+++ app.js
@@ -1,11 +1,49 @@
-const firstNumber = Number(prompt('Write first number'));
-console.log(typeof firstNumber, firstNumber);
+const age = Number(prompt('Скільки вам років?'));
+const city = prompt('З якого ви міста?');
 
-const secondNumber = Number(prompt('Write second number'));
-console.log(typeof secondNumber, secondNumber);
+const ua = 'України';
+const en = 'Лондона';
+const usa = 'США';
 
-const thirdNumber = Number(prompt('Write third number'));
-console.log(typeof thirdNumber, thirdNumber);
+if (city == 'Київ') {
+	alert(`Вая! Тобі вже ${age} і ти досі живеш у столиці ${ua}?`);
+} else if (city == 'Лондон') {
+	alert(`Вам ${age}, і Ви живете у столиці ${en}.`);
+} else if (city == 'Вашингтон') {
+	alert(
+		`Greetings, Mr. President! You're ${age} age piece of democracy from ${usa}!`
+	);
+} else {
+	alert(`
+	You're age: ${age}.
+	You're city: ${city}
+	`);
+}
 
-const average = (firstNumber + secondNumber + thirdNumber) / 3;
-console.log(typeof average, average);
+let bascketball = 'Bascketball';
+const bascketballPlayer = 'Lebron James';
+
+let football = 'Football';
+const footballPlayer = 'Zineddin Zidan';
+
+let racing = 'Racing';
+const racer = 'Michael Schumacher';
+
+const sportChoosing = prompt(
+	`Pick one: ${bascketball}, ${football}, ${racing}`
+);
+
+if (bascketball == 'Bascketball') {
+	alert(`So, you wanna b like ${bascketballPlayer}, honey?`);
+} else if (football == 'Football') {
+	alert(`Big power, it's a big responsibility. 
+		  (c)${footballPlayer}
+
+		 p.s.: Nah! It was a Peter Parkers uncle.`);
+} else if (racing == 'Racing') {
+	alert(`Wanna drive fast as ${racer}? Keep save)`);
+} else {
+	alert(`
+	You're sport: ${sportChoosing}.
+	`);
+}
